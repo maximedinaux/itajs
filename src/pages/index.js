262e3381fb84002da86abac928e2677e08057739
maximedinaux <maxime@dinaux.be>
2025-12-02 17:25:37 +0100

    v11

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const Index = () => {
     <div>
       <h1>Home</h1>
       <h2>
-        Test <strong>v8</strong> request
+        Test <strong>v11</strong> request
       </h2>
       <ul>
         {data.a.nodes.map((item, index) => {
