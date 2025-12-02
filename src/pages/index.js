0c5507618d6d2c0b3d9a09b324a19b4d010784c8
maximedinaux <maxime@dinaux.be>
2025-12-02 17:07:40 +0100

    first request

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const getData = graphql`
+  {
+    a: allWpPage {
+      nodes {
+        title
+      }
+    }
+  }
+`
 
 const Index = () => {
-  return <div>Index</div>
+  const data = useStaticQuery(getData)
+  return (
+    <div>
+      <h1>Home</h1>
+      <h2>Test request</h2>
+      <ul>
+        {data.a.nodes.map((item, index) => {
+          return <li kye={index}>{item.title}</li>
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Index
